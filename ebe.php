300d589dc23a7f261f1796f7c88740342debdf0f
Lamas Mendoza Dulce Ebenezer <161983102+Ebelamas@users.noreply.github.com>
2024-03-05 08:05:38 -0800

    Add files via upload

---
+++ ebe.php
@@ -0,0 +1,35 @@
+<?php
+
+$makeup = [
+    "Paleta de sombra" => "Eyeshadow Palette"
+"Polvo Suelto" => "polvo para cara"
+"Little Angel Dewy Hydrating" => "hidratante"
+"Colorete en Polvo Strawberry Rococo" => "polvo"
+" Colorete Swan" => "polvo swan"
+"Strawberry 5 Color Eyeshadow" => "polvo ice"
+"Polvo Suelto Swan Ballet Loose Powder" => "polvo ballet"
+" Little Angel Dewy" => " litte"
+"Angel Cream Blusher " => "cream bush"
+"- Brillo Labial" => "labial"
+];
+
+$precios = [
+"Paleta de sombra" => "646.20 Pesos"
+"Polvo Suelto" => "646.20 Pesos"
+"Little Angel Dewy Hydrating" => "646.20Pesos"
+"Colorete en Polvo Strawberry Rococo" => "480.03 Pesos"
+" Colorete Swan" => "480.03 Pesos"
+"Strawberry 5 Color Eyeshadow" => "646.20 Pesos"
+"Polvo Suelto Swan Ballet Loose Powder" => "646.20 Pesos"
+"Little Angel Dewy " => "646.20Pesos"
+"Angel Cream Blusher " => "480.03Pesos"
+"Brillo labial " => "406.18 Pesos"
+];
+
+$FlowerKnows = [
+ $makeup,
+ $precios
+];
+
+echo json_encode($FlowerKnows);
+?>
