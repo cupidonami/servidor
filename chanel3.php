af20cc8bfc8116c50d23fed7ebd0fe3042309892
hhyuumii <162342356+hhyuumii@users.noreply.github.com>
2024-03-05 07:56:31 -0800

    Add files via upload

---
+++ chanel3.php
@@ -0,0 +1,35 @@
+<?php
+
+$makeup = [
+    "Base" => "Revitalizante iluminador"
+"Corrector" => "correcteur"
+"delineador" => "Corrector Instant Age Rewind"
+"iluminador" => "les beigue"
+"Mascara de pestañas" => "nor alluire"
+"polvo suelto" => "poudre universelle"
+"Labial" => "Le rougue duo"
+"sombra de ojos" => "Les 4 ombre"
+"Balsamo labial" => "Hydra beauty"
+"Rubor en polvo" => "joues contraste"
+];
+
+$precios = [
+"Base" => "1,500"
+"corrector" => "2,700"
+"Delineador" => "121 Pesos"
+"Iluminador" => "789 Pesos"
+"Mascara de pestanas" => "746 Pesos"
+"polvo suelto" => "859 Pesos"
+"labial" => "800 Pesos"
+"Sombra de ojos" => "1,300 Pesos"
+"balsamo labial" => "600 Pesos"
+"Rubor en polvo" => "2,400 Pesos"
+];
+
+$Chanel= [
+ $makeup,
+ $precios
+];
+
+echo json_encode($chanel);
+?>
