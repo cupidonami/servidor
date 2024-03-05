e1468fd9e5c6e553258c5e25e3d6ff43209abccc
hhyuumii <162342356+hhyuumii@users.noreply.github.com>
2024-03-05 08:23:40 -0800

    Update chanel3.php

--- chanel3.php
+++ chanel3.php
@@ -1,34 +1,34 @@
 <?php
 
 $makeup = [
-    "Base" => "Revitalizante iluminador"
-"Corrector" => "correcteur"
-"delineador" => "Corrector Instant Age Rewind"
-"iluminador" => "les beigue"
-"Mascara de pestañas" => "nor alluire"
-"polvo suelto" => "poudre universelle"
-"Labial" => "Le rougue duo"
-"sombra de ojos" => "Les 4 ombre"
-"Balsamo labial" => "Hydra beauty"
-"Rubor en polvo" => "joues contraste"
+    "Base" => "Revitalizante iluminador",
+"Corrector" => "correcteur",
+"delineador" => "Corrector Innstant Age Rewind",
+"iluminador" => "les beigue",
+"Mascara de pestañas" => "nor alluire",
+"polvo suelto" => "poudre universelle",
+"Labial" => "Le rougue duo",
+"sombra de ojos" => "Les 4 ombre",
+"Balsamo labial" => "Hydra beauty",
+"Rubor en polvo" => "joues contraste",
 ];
 
-$precios = [
-"Base" => "1,500"
-"corrector" => "2,700"
-"Delineador" => "121 Pesos"
-"Iluminador" => "789 Pesos"
-"Mascara de pestanas" => "746 Pesos"
-"polvo suelto" => "859 Pesos"
-"labial" => "800 Pesos"
-"Sombra de ojos" => "1,300 Pesos"
-"balsamo labial" => "600 Pesos"
-"Rubor en polvo" => "2,400 Pesos"
+$valor = [
+"Base" => 1.500,
+"corrector" => 2.700,
+"Delineador" => 121,
+"Iluminador" => 789,
+"Mascara de pestanas" => 746,
+"polvo suelto" => 859 ,
+"labial" => 800,
+"Sombra de ojos" => 1.300 ,
+"balsamo labial" => 600,
+"Rubor en polvo" => 2.400,
 ];
 
 $Chanel= [
  $makeup,
- $precios
+ $valor
 ];
 
 echo json_encode($chanel);
